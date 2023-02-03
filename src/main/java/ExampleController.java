2b92ad3b2f0762a2aff6014a6117b5d39a61bdeb
Vu0811 <thevuu2001@gmail.com>
2023-02-03 13:42:32 +0700

    thevuu: add ExampleController

---
+++ src/main/java/ExampleController.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ExampleController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}

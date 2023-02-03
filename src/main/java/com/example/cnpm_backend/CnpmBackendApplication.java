d1023e5708049154077dc525630e52f4886ff267
Vu0811 <thevuu2001@gmail.com>
2023-02-03 13:38:47 +0700

    Initial Commit

---
+++ src/main/java/com/example/cnpm_backend/CnpmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnpm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnpmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnpmBackendApplication.class, args);
+    }
+
+}

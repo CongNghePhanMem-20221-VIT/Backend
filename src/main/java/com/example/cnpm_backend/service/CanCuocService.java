60b0d33177b197c5f5c7fd866d3b97372e939d8c
Vu0811 <thevuu2001@gmail.com>
2023-02-09 16:08:11 +0700

    thevu: fix Services

--- src/main/java/com/example/cnpm_backend/service/CanCuocService.java
+++ src/main/java/com/example/cnpm_backend/service/CanCuocService.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CanCuocService extends JpaRepository<CanCuocModel, Long> {
+public interface CanCuocService extends JpaRepository<CanCuocModel, Integer> {
 }

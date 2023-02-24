7e942d70a364c5f8d1fc9bedf38aad5e2a55028d
haopd12 <hao.pd200200@sis.hust.edu.vn>
2023-02-24 16:50:45 +0700

    haopd: Search Magiadinh

--- src/main/java/com/example/cnpm_backend/service/GiaDinhService.java
+++ src/main/java/com/example/cnpm_backend/service/GiaDinhService.java
@@ -1,9 +1,16 @@
 package com.example.cnpm_backend.service;
 
 import com.example.cnpm_backend.model.GiaDinhModel;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GiaDinhService extends JpaRepository<GiaDinhModel, Integer> {
+    @Query(value = "select * from giadinh gd where gd.Magiadinh like %:name%", nativeQuery = true)
+    List<GiaDinhModel> findGiaDinhByMa(@Param("name") String name);
 }
